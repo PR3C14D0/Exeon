87c94e352b56ad5a48016a3e27403f42fdc86b07
Preciado <aarongarpre@gmail.com>
2024-01-05 16:05:17 +0100

    Added DebugConsole

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,15 @@ bool g_bQuit = false;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+void DebugConsole();
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+#ifndef NDEBUG
+	DebugConsole();
+#endif // NDEBUG
+
+	std::cout << "Exeon debug console" << std::endl;
+
 	const char CLASS_NAME[] = "Exeon";
 	const char WINDOW_NAME[] = "Exeon";
 
@@ -30,14 +38,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	if (hwnd)
 		ShowWindow(hwnd, nShowCmd);
+	std::cout << "[DEBUG] Window created" << std::endl;
 
 	Core* core = Core::GetInstance();
 	core->SetHWND(hwnd);
 
 	core->Init();
-	
+	std::cout << "[DEBUG] Core initialized" << std::endl;
+
 	MSG msg = { };
 
+	std::cout << "[DEBUG] Starting main loop" << std::endl;
 	while (!g_bQuit) {
 		while (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
 			TranslateMessage(&msg);
@@ -48,6 +59,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	return FALSE;
+}
+
+void DebugConsole() {
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
