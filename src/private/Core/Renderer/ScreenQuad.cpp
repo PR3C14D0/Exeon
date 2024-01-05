50e95e5fcb26e6ffcf6d656fdfbe01b38231d51f
Preciado <aarongarpre@gmail.com>
2024-01-05 18:41:41 +0100

    Created ScreenQuad VBO and IBO

--- src/private/Core/Renderer/ScreenQuad.cpp
+++ src/private/Core/Renderer/ScreenQuad.cpp
@@ -3,12 +3,52 @@
 
 ScreenQuad::ScreenQuad() {
 	this->m_core = Core::GetInstance();
+	this->m_renderer = nullptr;
 }
 
 void ScreenQuad::Init() {
+	this->m_renderer = this->m_core->GetRenderer();
+
+	this->m_vertices = {
+		{{-1.f, 1.f, 0.f}, {0.f, 0.f}},
+		{{1.f, 1.f, 0.f}, {1.f, 0.f}},
+		{{1.f, -1.f, 0.f}, {1.f, 1.f}},
+		{{-1.f, -1.f, 0.f}, {0.f, 1.f}},
+	};
+
+	this->m_indices = {
+		0, 1, 3,
+		3, 1, 2
+	};
+	
+	if (D3D12* renderer = dynamic_cast<D3D12*>(this->m_renderer)) {
+		this->D3D12Init(renderer);
+	}
 
 }
 
-void ScreenQuad::Render() {
+void ScreenQuad::D3D12Init(D3D12* renderer) {
+	renderer->GetDevice(this->m_dev);
+	renderer->GetCommandList(this->m_list);
 
+	renderer->CreateBuffer(&this->m_vertices[0], this->m_vertices.size() * sizeof(Vertex), this->m_VBO);
+	renderer->CreateBuffer(&this->m_indices[0], this->m_indices.size() * sizeof(UINT), this->m_IBO);
+
+	this->m_VBV.BufferLocation = this->m_VBO->GetGPUVirtualAddress();
+	this->m_VBV.SizeInBytes = this->m_vertices.size() * sizeof(Vertex);
+	this->m_VBV.StrideInBytes = sizeof(Vertex);
+
+	this->m_IBV.BufferLocation = this->m_IBO->GetGPUVirtualAddress();
+	this->m_IBV.Format = DXGI_FORMAT_R32_UINT;
+	this->m_IBV.SizeInBytes = this->m_indices.size() * sizeof(UINT);
+}
+
+void ScreenQuad::Render() {
+	if (D3D12* renderer = dynamic_cast<D3D12*>(this->m_renderer)) {
+		this->D3D12Render(renderer);
+	}
+} 
+
+void ScreenQuad::D3D12Render(D3D12* renderer) {
+	
 }
--- src/public/Core/Renderer/ScreenQuad.h
+++ src/public/Core/Renderer/ScreenQuad.h
@@ -3,14 +3,34 @@
 #include <Windows.h>
 #include <d3d12.h>
 #include <wrl.h>
+#include <vector>
+#include "Util.h"
 
 using namespace Microsoft::WRL;
 
 class Core;
+class Renderer;
+class D3D12;
 
 class ScreenQuad {
 private:
 	Core* m_core;
+	void D3D12Init(D3D12* renderer);
+	void D3D12Render(D3D12* renderer);
+
+	Renderer* m_renderer;
+	
+	std::vector<Vertex> m_vertices;
+	std::vector<UINT> m_indices;
+
+	ComPtr<ID3D12Resource> m_VBO;
+	ComPtr<ID3D12Resource> m_IBO;
+
+	D3D12_VERTEX_BUFFER_VIEW m_VBV;
+	D3D12_INDEX_BUFFER_VIEW m_IBV;
+
+	ComPtr<ID3D12Device1> m_dev;
+	ComPtr<ID3D12GraphicsCommandList> m_list;
 public:
 	ScreenQuad();
 	
