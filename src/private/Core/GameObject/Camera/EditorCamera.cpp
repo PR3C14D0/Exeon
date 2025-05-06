0a18098a11fa09e2826db451433047d2bc940243
PR3C14D0 <aarongarpre@gmail.com>
2025-05-06 06:40:19 +0200

    EditorCamera: Finished input system.

--- src/private/Core/GameObject/Camera/EditorCamera.cpp
+++ src/private/Core/GameObject/Camera/EditorCamera.cpp
@@ -6,8 +6,6 @@ EditorCamera::EditorCamera(std::string name) : Camera(name) {
 
 void EditorCamera::Init() {
 	Camera::Init();
-
-
 }
 
 void EditorCamera::Update() {
@@ -16,7 +14,23 @@ void EditorCamera::Update() {
 	if(this->m_input->GetButtonDown(MOUSE_BUTTON::RIGHT)) {
 		this->m_input->ShowCursor(false);
 		
+		this->transform.Rotate(this->m_input->deltaY, this->m_input->deltaX, 0.f);
 
+		if (this->m_input->GetKeyDown('w')) {
+			this->transform.Translate(this->transform.Forward() * 0.1f);
+		}
+
+		if (this->m_input->GetKeyDown('s')) {
+			this->transform.Translate(this->transform.Forward() * -0.1f);
+		}
+
+		if (this->m_input->GetKeyDown('d')) {
+			this->transform.Translate(this->transform.Right() * 0.1f);
+		}
+
+		if (this->m_input->GetKeyDown('a')) {
+			this->transform.Translate(this->transform.Right() * -0.1f);
+		}
 	}
 	else {
 		this->m_input->ShowCursor(true);
